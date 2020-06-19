d2673b33dd8fec390d8ffb6b29d8c8d8b901c57d
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-18 17:39:27 -0700

    deleted options model and used aws to store photos

--- server/database/seed.js
+++ server/database/seed.js
@@ -11,16 +11,16 @@ const capitalize = (word) => {
 
 const getImage = () => {
   const index = Math.floor(getRandomInt(0, 10))
-  const images = ['https://images-na.ssl-images-amazon.com/images/I/71ZV8tYG%2BnL._SL1500_.jpg',
-                  'https://jilt.com/wp-content/uploads/2019/06/fenty-featured.jpg',
-                  'https://s1.thcdn.com/productimg/1600/1600/11752246-3294597285458378.jpg',
-                  'https://i5.walmartimages.com/asr/b8a1e0f8-7154-4a69-af22-f53b9970f291_3.600ef75af25b4fadb9aca2a6f96454a0.jpeg',
-                  'https://www.narscosmetics.com/dw/image/v2/BBSK_PRD/on/demandware.static/-/Sites-itemmaster_NARS/default/dw51a276cc/hi-res/0607845012344.jpg?sw=856&sh=750&sm=fit',
-                  'https://images.ulta.com/is/image/Ulta/2548164?op_sharpen=1&resMode=bilin&qlt=85&wid=800&hei=800&fmt=jpg',
-                  'https://cdn.shopify.com/s/files/1/2037/5105/products/KKW-Nude-Lipstick-1_1024x.jpg?v=1562091300',
-                  'https://images.ulta.com/is/image/Ulta/2539520?op_sharpen=1&resMode=bilin&qlt=85&wid=800&hei=800&fmt=jpg',
-                  'https://images.ulta.com/is/image/Ulta/2242417?op_sharpen=1&resMode=bilin&qlt=85&wid=800&hei=800&fmt=jpg',
-                  'https://www.sephora.com/productimages/sku/s1721240-main-hero.jpg']
+  const images = ['https://sephjen.s3-us-west-1.amazonaws.com/sephora1.jpg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/sephora2.jpg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/Sephora3.jpg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/sophora4.jpeg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/sephora5.jpg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/sophora6.jpeg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/sephora7.jpg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/sephora8.jpeg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/sephora9.jpeg',
+                  'https://sephjen.s3-us-west-1.amazonaws.com/sephora10.webp']
   return images[index];
 }
 
@@ -52,7 +52,7 @@ const getSize = () => {
   }
 }
 
-for (var i = 0; i < 99; i++) {
+for (var i = 0; i < 100; i++) {
   Product.create({
     image: getImage(), 
     brand: capitalize(faker.lorem.word()), 
