060f7cb5999534a40198ce69d774935c0a8fd6ba
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-19 11:04:36 -0700

    served static file

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express()
 const Product = require('./models/product.js')
 const port = 3000
 
+app.use(express.static('../client/dist'))
+
 app.get('/product', (req, res) => {
   Product.find()
     .then((products) => res.send(products))
