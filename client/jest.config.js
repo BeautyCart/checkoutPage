452b735479b941cd2c19496881b29e78a0ecca7a
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-21 23:23:32 -0700

    added to json as well as make the jest config file

---
+++ client/jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  displayName: 'components',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/test/**/*.test.js'],
+  setupFilesAfterEnv: ['<rootDir>/test/setupTests.js'],
+  maxConcurrency: 5,
+  moduleNameMapper: {
+    '\\.(png)$': '../../test/mocks/fileMock.js'
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
+  coveragePathIgnorePatterns: ['src/index.js'],
+  coverageThreshold: {
+    'global': {
+      'branches': 100,
+      'functions': 100,
+      'lines': 100,
+      'statements': 100
+    }
+  }
+};
