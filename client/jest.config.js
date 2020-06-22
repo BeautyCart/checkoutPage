ca753e71decadbc8197eed75088b6ee088356de2
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-22 11:52:06 -0700

    got test to work

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  displayName: 'components',
-  testEnvironment: 'jsdom',
-  testMatch: ['**/test/**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/test/setupTests.js'],
-  maxConcurrency: 5,
-  moduleNameMapper: {
-    '\\.(png)$': '../../test/mocks/fileMock.js'
-  },
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  },
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js'],
-  coveragePathIgnorePatterns: ['src/index.js'],
-  coverageThreshold: {
-    'global': {
-      'branches': 100,
-      'functions': 100,
-      'lines': 100,
-      'statements': 100
-    }
-  }
-};
+  setupFiles: ['<rootDir>/tests/setupTests.js']
+};  
+// displayName: 'omponents',
+// testEnvironment: 'jsdom',
+// testMatch: ['**/test/**/*.test.js'],
+// setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
+// maxConcurrency: 5,
+// moduleNameMapper: {
+//   '\\.(png)$': '../../test/mocks/fileMock.js'
+// },
+// transform: {
+//   '^.+\\.js$': 'babel-jest'
+// },
+// collectCoverage: true,
+// collectCoverageFrom: ['src/**/*.js'],
+// coveragePathIgnorePatterns: ['src/index.js'],
+// coverageThreshold: {
+//   'global': {
+//     'branches': 100,
+//     'functions': 100,
+//     'lines': 100,
+//     'statements': 100
+//   }
+// }
