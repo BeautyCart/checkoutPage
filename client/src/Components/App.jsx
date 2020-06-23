c79bc3521b6a6ae07d5a4184cb58a5b7e126807f
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-22 21:55:02 -0700

    passed in random data to current item state

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ItemInfo from './ItemInfo.jsx';
-import ItemCheckout from './ItemCheckout.jsx'
-import Options from './Options.jsx'
+import ItemCheckout from './ItemCheckout.jsx';
+import Options from './Options.jsx';
+import axios from 'axios';
 
 class App extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      currentItem:{}
+    }
+    this.getProducts = this.getProducts.bind(this);
+    this.setRandomProduct = this.setRandomProduct.bind(this);
+    this.setCurrentItem = this.setCurrentItem.bind(this);
   }
+  
+  componentDidMount() {
+    console.log('component did mount')
+    this.getProducts(this.setRandomProduct);
+  }
+
+  getRandom(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  setCurrentItem(productData) {
+    this.setState({
+      currentItem: productData
+    })
+  }
+
+  setRandomProduct(productsData) {
+    const index = this.getRandom(0, productsData.length - 1);
+    this.setCurrentItem(productsData[index])
+  }
+
+  getProducts(func) {
+    axios.get('/product')
+      .then((response) => func(response.data))
+      .catch((err) => console.log('Error getting random product', err))
+  }
+
   render() {
     return (
       <div>
--- client/src/Components/Options.jsx
+++ client/src/Components/Options.jsx
@@ -4,20 +4,9 @@ import ReactDOM from 'react-dom';
 function Options(props) {
   return (
     <div>
-      <select>
-        <option value='1'>1</option>
-        <option value='2'>2</option>
-        <option value='3'>3</option>
-        <option value='4'>4</option>
-        <option value='5'>5</option>
-        <option value='6'>6</option>
-        <option value='7'>7</option>
-        <option value='8'>8</option>
-        <option value='9'>9</option>
-        <option value='10'>10</option>
-      </select>
-      <button>Add To Basket</button>
-      <button>Add To Loves</button>     
+      <div>Option Chosens</div>
+      <div>Option 1</div>
+      <div>Option 2</div>
     </div>
   )
 }
