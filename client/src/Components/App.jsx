6f212b6460692d200f342403028a3d2c41746324
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-23 11:56:19 -0700

    got the option button click to be handled properly

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -55,7 +55,7 @@ class App extends React.Component{
     return;
   }
   
-  handleOptionClick(option) {
+  handleOptionClick(index) {
     this.setOptionChosenIndex(index);
   }
 
--- client/src/Components/Options.jsx
+++ client/src/Components/Options.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Options(props) {
-  console.log('props', props)
+  // console.log('props', props)
+  const handleClick = (e) => {
+    // console.log('should log the value chosen', e.target.value)
+    props.handleOptionClick(e.target.value)
+  }
   let displayOptions;
   let displayOptionChosen;
   if (props.optionChosenIndex >= 0) {
-    console.log('should display option chosen', props.options[props.optionChosenIndex].amount)
+    // console.log('should display option chosen', props.options[props.optionChosenIndex].amount)
     displayOptionChosen = <p>SIZE: {props.options[props.optionChosenIndex].amount}</p>
   } else {
     displayOptionChosen = <p>Loading...</p>
   }
   if(props.options) {
-    displayOptions = props.options.map((option, index) => <button value={index}>{option.label} {option.amount}</button>)
+    displayOptions = props.options.map((option, index) => <button value={index} onClick={handleClick}>{option.label} {option.amount}</button>)
   } else {
     displayOptions = <div>Loading....</div>
   }
