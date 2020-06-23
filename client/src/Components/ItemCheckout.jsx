73f8c2a60af860ee4fa84f0ba57e29d57dfa8370
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-22 19:55:42 -0700

    put inside right component

--- client/src/Components/ItemCheckout.jsx
+++ client/src/Components/ItemCheckout.jsx
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 
 function ItemCheckout(props) {
   return (
-    <div>Hello ItemCheckout</div>
+    <div>
+      <select>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        <option value='6'>6</option>
+        <option value='7'>7</option>
+        <option value='8'>8</option>
+        <option value='9'>9</option>
+        <option value='10'>10</option>
+      </select>
+      <button>Add To Basket</button>
+      <button>Add To Loves</button>     
+    </div>
   )
 }
 
