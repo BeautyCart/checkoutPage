e011fe87dcbfef6a092b1a50b1c03ce83642a863
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-24 11:10:57 -0700

    started working on tests

--- client/__tests__/App.test.jsx
+++ client/__tests__/App.test.jsx
@@ -3,10 +3,20 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import App from '../src/Components/App';
 import ItemInfo from '../src/Components/ItemInfo';
+import ItemCheckout from '../src/Components/ItemCheckout';
+import Options from '../src/Components/Options';
 
 describe('<App />', () => {
   it('renders a <ItemInfo /> component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(ItemInfo)).to.have.lengthOf(1);
+  });
+  it('renders a <ItemCheckout /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(ItemCheckout)).to.have.lengthOf(1);
+  });
+  it('renders a <Options /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Options)).to.have.lengthOf(1);
   });
 });
