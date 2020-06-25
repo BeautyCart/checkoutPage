348128e18615f1708aca1770f562f875fb9518ff
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-24 22:10:20 -0700

    started writing a little bit of tests but realized i was on the master branch

--- client/__tests__/ItemInfo.test.jsx
+++ client/__tests__/ItemInfo.test.jsx
@@ -1,3 +1,19 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import ItemInfo from '../src/Components/ItemInfo';
+
+describe('<ItemInfo />', () => {
+  it('renders a <ItemInfo /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(ItemInfo)).to.have.lengthOf(1);
+  });
+  it('renders a <ItemCheckout /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(ItemCheckout)).to.have.lengthOf(1);
+  });
+  it('renders a <Options /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Options)).to.have.lengthOf(1);
+  });
+});
