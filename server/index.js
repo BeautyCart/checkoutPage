2faf1f05002eccd3ade58dfeda8d6f179eb54ff1
Jennifer Xiong <jenni4xiong@gmail.com>
2020-06-29 23:54:13 -0700

    changed server to 4000

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const Product = require('./models/product.js');
 
-const port = 3000;
+const port = 4000;
 
 app.use(express.static('../client/dist'));
 
